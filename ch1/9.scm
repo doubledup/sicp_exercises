ec6e6b918c5ebb76f42ef0db29a8525d0b69959c
David Dunn <26876072+doubledup@users.noreply.github.com>
2019-04-30 08:00:15 +0200

    Make 1.9 runnable

--- ch1/9.scm
+++ ch1/9.scm
@@ -1,7 +1,18 @@
-(define (+ a b)
+; Renamed + to add here so that we can run it. If we don't rename our +
+; procedure, we can't define inc without using the new definition of +, which
+; calls inc itself. Even defining add as below doesn't help, as this still
+; calls our newly defined +:
+; (define (add x y) (+ x y))
+
+(define (inc x) (+ x 1))
+(define (dec x) (- x 1))
+
+(define (add1 a b)
   (if (= a 0)
       b
-      (inc (+ (dec a) b))))
+      (inc (add1 (dec a) b))))
+
+(add1 4 5)
 
 ; > (+ 4 5)
 ; > (inc (+ 3 5))
@@ -13,15 +24,19 @@
 ; > (inc (inc 7))
 ; > (inc 8)
 ; > 9
+; This process is recursive.
 
-(define (+ a b)
+(define (add2 a b)
   (if (= a 0)
       b
-      (+ (dec a) (inc b))))
+      (add2 (dec a) (inc b))))
+
+(add2 4 5)
 
 ; > (+ 4 5)
 ; > (+ 3 6)
 ; > (+ 2 7)
 ; > (+ 1 8)
 ; > (+ 0 9)
-; > 0
+; > 9
+; This process is iterative.
